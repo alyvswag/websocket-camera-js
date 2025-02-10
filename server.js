46bd532fb609f3317ea2f7dd1b2982627d7603e4
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2025-02-10 18:20:13 +0400

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = new WebSocket.Server({ port: process.env.PORT || 3000 });
 let robots = {}; // {"robot1": [client1, client2], "robot2": [client3]}
 
 server.on("connection", (ws) => {
-    console.log("Yeni müştəri qoşuldu!");
+    console.log("New client connected!");
 
     let assignedRobot = null;
 
@@ -13,34 +13,33 @@ server.on("connection", (ws) => {
         try {
             let data = JSON.parse(message);
 
-            // Robot qeydiyyatı
             if (data.type === "register" && data.robotName) {
                 assignedRobot = data.robotName;
                 if (!robots[assignedRobot]) {
                     robots[assignedRobot] = [];
                 }
                 robots[assignedRobot].push(ws);
-                console.log(`Müştəri ${assignedRobot} robotuna qeydiyyatdan keçdi`);
-                ws.send(JSON.stringify({ type: "info", message: `Robot ${assignedRobot} ilə qeydiyyatdan keçdi` }));
-
-            } else if (data.type === "video" && data.robotName) {
-                // Canlı video frame-ləri almaq
-                console.log(`Canlı video frame-i: ${data.robotName}`);
-                // Burada base64 şəkli müştərilərə göndərin
+                console.log(`Client registered to robot: ${assignedRobot}`);
+                ws.send(JSON.stringify({ type: "info", message: `Registered to ${assignedRobot}` }));
+            }
+            else if (data.type === "command" && assignedRobot) {
+                console.log(`Command for ${assignedRobot}: ${data.command}`);
                 robots[assignedRobot].forEach(client => {
                     if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({ type: "video", data: data.data }));
+                        client.send(JSON.stringify({ type: "command", command: data.command }));
                     }
                 });
             }
-
+            else {
+                ws.send(JSON.stringify({ type: "error", message: "Invalid request" }));
+            }
         } catch (e) {
-            ws.send(JSON.stringify({ type: "error", message: "Yanlış JSON formatı" }));
+            ws.send(JSON.stringify({ type: "error", message: "Invalid JSON format" }));
         }
     });
 
     ws.on("close", () => {
-        console.log("Müştəri əlaqəni kəsdi");
+        console.log("Client disconnected");
         if (assignedRobot && robots[assignedRobot]) {
             robots[assignedRobot] = robots[assignedRobot].filter(client => client !== ws);
             if (robots[assignedRobot].length === 0) {
@@ -49,6 +48,4 @@ server.on("connection", (ws) => {
         }
     });
 });
-
-console.log(`WebSocket serveri portda ${process.env.PORT || 3000} başladıldı`);
 
