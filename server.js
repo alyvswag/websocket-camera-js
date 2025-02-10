82822f21fac215f4ae996d7f19ee8139055a0073
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2025-02-10 19:21:05 +0400

    test

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const WebSocket = require("ws");
 const express = require("express");
+const http = require("http");
 
 const app = express();
-const server = require("http").createServer(app);
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 let lastImage = null;
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", (ws) => {
     console.log("Yeni müştəri qoşuldu!");
 
-    // Əgər müştəri qoşularsa, ona sonuncu görüntünü göndər
     if (lastImage) {
         ws.send(lastImage);
     }
 
-    ws.on("message", function incoming(data) {
-        lastImage = data;  // Sonuncu şəkli yadda saxla
+    ws.on("message", (data) => {
+        lastImage = data;
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(data);
@@ -24,13 +24,13 @@ wss.on("connection", function connection(ws) {
         });
     });
 
-    ws.on("close", function () {
+    ws.on("close", () => {
         console.log("Müştəri bağlantısı kəsildi.");
     });
 });
 
-app.use(express.static("public")); // HTML səhifəsini göstərmək üçün
+app.use(express.static("public"));
 
-server.listen(9090, function () {
+server.listen(9090, () => {
     console.log("Server 9090 portunda işləyir...");
 });
